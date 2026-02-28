abd699581d8678e6dc87d4cd96a63691dd66692f
Jaival Pandya <jaivalpandya123@gmail.com>
2026-02-28 21:29:14 +0530

    Change font size of name in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -90,7 +90,7 @@ export default function About() {
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent opacity-80 group-hover:opacity-90 transition-opacity duration-500"></div>
               <div className="absolute bottom-0 left-0 right-0 p-8 text-white transform translate-y-2 group-hover:translate-y-0 transition-transform duration-500">
-                <p className="text-3xl font-serif italic mb-1">Pandit Prashant Pandya</p>
+                <p className="text-2xl font-serif italic mb-1">Pandit Prashant Pandya</p>
                 <p className="text-yellow-300 font-medium tracking-wide">Matrugaya Tirth Pandit</p>
               </div>
             </div>
